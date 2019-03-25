683a419d8ff99841c04dd5e7371a4df563304937
chainchompa <chainchompa@gmail.com>
2019-03-24 21:47:07 -0400

    changed navigation to a class

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,22 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-const Navigation = () => {
-  return (
-    <div className="navigation">
-      <div className="nav-links">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          About
-        </Link>
-        <Link to="/projects" style={{ textDecoration: "none" }}>
-          Projects
-        </Link>
-        <Link to="/contact" style={{ textDecoration: "none" }}>
-          Contact
-        </Link>
+class Navigation extends React.Component {
+constructor(props) {
+super(props);
+this.state = {
+    activePage: ""
+  }
+};
+  render() {
+    return (
+      <div className="navigation">
+        <div className="nav-links">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            About
+          </Link>
+          <Link to="/projects" style={{ textDecoration: "none" }}>
+            Projects
+          </Link>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            Contact
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Navigation;
