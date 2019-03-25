3deeef078dccf6f73e49e8edebfed0aeaae693d9
chainchompa <chainchompa@gmail.com>
2019-03-24 21:36:50 -0400

    Updates

---
+++ precache-manifest.ff9802f7ac47db6534f424cd31be8a8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d941232120af51013cc42aa73194c6",
+    "url": "/static/media/notes.f2d94123.png"
+  },
+  {
+    "revision": "af643d927a6743a233d2e001e66ae6a8",
+    "url": "/static/media/gameoflife.af643d92.png"
+  },
+  {
+    "revision": "ad8d3ebf4b4fa643a47d352eff601b64",
+    "url": "/static/media/architects.ad8d3ebf.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aa7c5141d6e79eb0b2cd",
+    "url": "/static/js/main.aa7c5141.chunk.js"
+  },
+  {
+    "revision": "2ffeea37b8cea0aa8331",
+    "url": "/static/js/1.2ffeea37.chunk.js"
+  },
+  {
+    "revision": "aa7c5141d6e79eb0b2cd",
+    "url": "/static/css/main.8b8e2a1e.chunk.css"
+  },
+  {
+    "revision": "5b3efe83464b0ed37561028aced9e209",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f09581090b5d93ffabddd67d878337d1.js"
+  "/precache-manifest.ff9802f7ac47db6534f424cd31be8a8d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
